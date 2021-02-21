260bccf7898180b73fbccb696455f68cab6d65e1
dimon <dimon.pij@mail.ru>
2021-02-21 22:42:40 +0300

    add custom tags

--- src/main/java/com/pozharsky/dmitri/util/FormatCurrencyUtil.java
+++ src/main/java/com/pozharsky/dmitri/util/FormatCurrencyUtil.java
@@ -6,14 +6,14 @@ import java.text.NumberFormat;
 import java.util.Locale;
 
 public final class FormatCurrencyUtil {
-    private static final double COURSE = 2.6;
+    private static final double COURSE_BY_USD = 2.6;
 
     private FormatCurrencyUtil() {
     }
 
     public static String formatCurrency(BigDecimal value, Locale locale) {
         if (locale.equals(Locale.US)) {
-            value = value.divide(new BigDecimal(COURSE), RoundingMode.HALF_UP);
+            value = value.divide(new BigDecimal(COURSE_BY_USD), RoundingMode.HALF_UP);
         }
         NumberFormat numberFormat = NumberFormat.getCurrencyInstance(locale);
         return numberFormat.format(value);
